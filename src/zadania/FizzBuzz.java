0afc14c1bfbacf21ddf2b0fa7bd22b471802fd5c
Tomas <tomas.hulina@student.tuke.sk>
2023-08-30 20:14:21 +0200

    pridanie fizzbuzz verzie od copilota

--- src/zadania/FizzBuzz.java
+++ src/zadania/FizzBuzz.java
@@ -1,37 +1,50 @@
 package zadania;
 
-import java.text.Normalizer;
-import java.util.Scanner;
-
 public class FizzBuzz {
 
-    public static void main(String[] args) {
+  /*
 
-        int cislo = 1;
-        for (;;) {
-            if (cislo%3 == 0 && cislo%5 == 0 ){
-                System.out.println("Fizz Buzz");
-            } else if (cislo%3 == 0) {
+  ////////Copilot riesenie
+
+  public static void fizzBuzz(int n) {
+        for (int i=1;i<=n;i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (cislo%5 == 0) {
+                continue;
+            }
+            if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }  else {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }*/
+
+    public static void main(String[] args) {
+        /////fizzBuzz(100);
+
+
+        //moje riesenie
+        int cislo = 1;
+        do {
+            if (cislo % 3 == 0 && cislo % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (cislo % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (cislo % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
                 System.out.println(cislo);
             }
             cislo++;
-            if (cislo > 100){
-                break;
-            }
-        }
+        } while (cislo <= 100);
+
 
 
     }
-
-
-
-
-
-
-
 
 }
